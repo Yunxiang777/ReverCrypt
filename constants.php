0739cb873ff0c81e412266331c00662788fb2189
Yunxiang777 <yunxiangwang777@gmail.com>
2024-07-03 10:44:36 +0800

    add: constant資料庫常數

---
+++ constants.php
@@ -0,0 +1,7 @@
+<?php
+// Database configuration constants
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'mydb');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_PORT', '3307');
